bed1f7d69a70afc4f3e7e670f369ec0fba7e490a
rkorzen <korzeniewski@gmail.com>
2024-09-17 15:12:42 +0200

    json

--- cwiczenie_09.py
+++ cwiczenie_09.py
@@ -18,3 +18,25 @@ a: 11, b: 11
 a: 11, b: 13
 
 """
+import json
+
+try:
+    with open("data/ddd.json") as f:
+        dane = json.load(f)
+except FileNotFoundError:
+    dane = []
+
+
+tryb = input('Odczyt czy zapis [o, z]')
+
+if tryb == "o":
+    for d in dane:
+        print(f"a: {d['a']}, b: {d['b']}")
+if tryb == "z":
+    a = int(input("podaj a: "))
+    b = int(input("podaj b: "))
+    d = {"a": a, "b": b}
+    dane.append(d)
+
+    with open("data/ddd.json", "w") as f:
+        json.dump(dane, f)
